60d7832d0688b9cc178f85fb8289484defb075a4
Jim Wilson <Jim Wilson>
2016-08-23 13:33:42 -0400

    div by 3 = mined div by 5 minds div by both mined minds

--- printtoscreen.rb
+++ printtoscreen.rb
@@ -1,6 +1,11 @@
-10.times do
-	puts 1
+number = 1
+100.times do 
+	if number % 3 == 0
+		puts "Mined"
+	elsif number % 5 == 0
+		puts "Minds"
+	else
+		puts number
+	end
+	number = number + 1
 end
-
-# puts "1" * 20
-
