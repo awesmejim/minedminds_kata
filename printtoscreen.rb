a1248cc9b03a5205de1621f8f17bd41944018180
Jim Wilson <Jim Wilson>
2016-08-23 14:38:06 -0400

    add test 1 returns 1

--- printtoscreen.rb
+++ printtoscreen.rb
@@ -1,6 +1,8 @@
 number = 1
-100.times do 
-	if number % 3 == 0
+100.times do  
+	if number % 15 == 0
+		puts "MinedMinds"
+	elsif number % 3 == 0
 		puts "Mined"
 	elsif number % 5 == 0
 		puts "Minds"
