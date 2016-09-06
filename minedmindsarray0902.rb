add6e5d2060a106829a425c754100ebb0a5c3cab
Jim Wilson <Jim Wilson>
2016-09-06 09:51:04 -0400

    1st Ruby Lesson!

--- minedmindsarray0902.rb
+++ minedmindsarray0902.rb
@@ -1,8 +1,20 @@
 def array_mined_minds
 	my_array = []
 	(1..100).each do |number|
-		number = number
+		if number = number
 		my_array << number
 		end
 	my_array
 end
+
+
+
+
+
+
+
+# 	else number = number
+# 	end
+#   my_array << number
+# 	end
+	
